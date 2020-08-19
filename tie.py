ea1a16d926f5861f4053a777230af19771e7cfde
javurena7 <javurena7@gmail.com>
2020-08-19 17:41:29 +0300

    Add tie-level code sketch

---
+++ tie.py
@@ -0,0 +1,40 @@
+import numpy as np
+from analytics import *
+import matplotlib.pyplot as plt; plt.ion()
+
+
+class TieAnalytics(object):
+    def __init__(self, nodes, times=None, query=''):
+        """
+        times - dict with
+        """
+        self.nodes = nodes
+        if times is None:
+            assert query, "times or query must be provided"
+            self.times = self._query_to_times(query)
+        else:
+            self.times = times
+        self.stats = {}
+
+
+
+    def _query_to_times(self, query):
+
+        pass
+
+
+    def get_iet_distribution(self):
+        pass
+
+
+    def get_overlap(self):
+        pass
+
+
+    def get_bursty_cascades(self, dt=365):
+        pass
+
+
+    def get_reciprocity(self):
+        pass
+
